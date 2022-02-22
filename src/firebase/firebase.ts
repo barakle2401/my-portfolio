be5e91619035ef68286b202c9073053ba4a8c098
Barak Levy <50266679+barakle2401@users.noreply.github.com>
2022-02-22 11:11:41 +0200

    Update firebase.ts

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA4_P9D13V3rKwm9KFLGVHFjWD4wXkfeVQ',
-  authDomain: 'dogs-f6ab2.firebaseapp.com',
-  projectId: 'dogs-f6ab2',
-  storageBucket: 'dogs-f6ab2.appspot.com',
-  messagingSenderId: '169864033183',
-  appId: '1:169864033183:web:242905bf2dfe414c3543aa',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 export const app = initializeApp(firebaseConfig);
